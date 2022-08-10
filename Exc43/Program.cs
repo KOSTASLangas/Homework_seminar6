d6853061152fcdf1d26f4b7e75246e9310bbc2b3
Kostas <langas.kostya@mail.ru>
2022-08-10 19:06:23 +0500

    Нахождение точки пересечения двух прямых

---
+++ Exc43/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+double k1 = Convert.ToInt32(Console.ReadLine());
+double b1 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
+double b2 = Convert.ToInt32(Console.ReadLine());
+double a = 0;
+double c = 0;
+double x = 0;
+double y = 0;
+a = k1 - k2;
+c = b1 - b2;
+x = (c * -1) / a;
+y = (k2 * x) + b2;
+Console.WriteLine( $"({x}, {y})");
